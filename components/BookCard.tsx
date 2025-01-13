0c51d00f944bbf408b2f4d46d9a4d544cd769ea5
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-01-13 07:00:57 +0200

    files updated

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import Link from "next/link";
+import BookCover from "@/components/BookCover";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-const BookCard = () => {
-  return <div>BookCard</div>;
-};
+const BookCard = ({
+  id,
+  title,
+  genre,
+  coverColor,
+  coverUrl,
+  isLoanedBook = false,
+}: Book) => (
+  <li className={cn(isLoanedBook && "xs:w-52 w-full")}>
+    <Link
+      href={`/books/${id}`}
+      className={cn(isLoanedBook && "w-full flex flex-col items-center")}
+    >
+      <BookCover coverUrl={coverUrl} coverImage={coverUrl} />
+
+      <div className={}></div>
+    </Link>
+  </li>
+);
 export default BookCard;
